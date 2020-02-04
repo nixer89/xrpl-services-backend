8556cac7011ef4cc6222d338b5fa150409125b27
nixer89 <pbnixer@gmail.com>
2020-02-04 13:20:05 +0100

    removed old config params

--- config.ts
+++ config.ts
@@ -1,4 +1,2 @@
-export const XUMM_API_URL = 'https://xumm.app/api/v1/platform/';
-export const XUMM_APP_ID = process.env.XUMM_APP_ID
-export const XUMM_APP_SECRET = process.env.XUMM_APP_SECRET
+export const XUMM_API_URL = process.env.XUMM_API_URL || 'https://xumm.app/api/v1/platform/';
 export const BITHOMP_API_TOKEN = process.env.BITHOMP_API_TOKEN;
